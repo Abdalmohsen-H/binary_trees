1900bb85f4775e34d4d9936a0ea80530f1254f02
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-31 19:19:45 -0700

    task 10 : depth of node in binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Func. to calculate depth of a node in a binary tree
+ * @tree: Pointer to the node in binary tree to measure the depth of it
+ * Return: Depth of node in b-tree or 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	/*condition to break recursion*/
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	/*continue recursion, Find depth of a node in a binary tree*/
+	return (1 + binary_tree_depth(tree->parent));
+}
